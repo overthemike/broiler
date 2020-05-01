8c845e794038d21482261c78cd60199fe3fbbe1d
Michael Sweeney <jfmike@gmail.com>
2020-05-01 02:36:38 -0700

    got rid of foo

--- foo/client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
-import 'normalize.css/normalize.css'
-import 'font-awesome/css/font-awesome.min.css'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
